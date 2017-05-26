d81e27989d8343f5c57acfdd1c6fdfe90182658a
Or Mantzur <or.mantzur@gmail.com>
2017-05-26 14:51:37 +0300

    car basic

---
+++ Ex03.GarageLogic/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Car
+    {
+        enum eColor
+        {
+            Yellow,
+            White,
+            Black,
+            Blue
+        }
+
+        private eColor m_Color;
+        private byte m_NumDoors;
+    }
+}
