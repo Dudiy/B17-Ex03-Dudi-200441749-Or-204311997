b116adb3f4d6703828904592751cf0373f72af21
Dudiy <dudi.2705@gmail.com>
2017-05-26 14:49:34 +0300

    Initializing classes

---
+++ Ex03.GarageLogic/Bike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Bike : Vehicle
+    {
+        eLicenseType m_LicenseType;
+        int m_EngineCapacity;
+
+        public Bike(string i_LicensePlate, string i_ModelName,)
+            :base()
+        {
+        }
+    }
+}
