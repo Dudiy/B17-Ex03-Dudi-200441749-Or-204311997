026aaa2732d26b6ac27fd95c22f194b225dea971
Or Mantzur <or.mantzur@gmail.com>
2017-05-26 16:25:52 +0300

    car

--- Ex03.GarageLogic/Car.cs
+++ Ex03.GarageLogic/Car.cs
@@ -4,9 +4,15 @@ using System.Text;
 
 namespace Ex03.GarageLogic
 {
-    class Car
+    class Car : Vehicle
     {
         private eColor m_Color;
         private byte m_NumDoors;
+
+        public Car(string i_LicensePlate, eColor i_Color, byte i_NumDoors) : base(i_LicensePlate)
+        {
+            m_Color = i_Color;
+            m_NumDoors = i_NumDoors;
+        }
     }
 }
