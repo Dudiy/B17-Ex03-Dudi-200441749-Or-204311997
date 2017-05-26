fa25aff5f6a40715618836f889b124b43e61e973
Dudiy <dudi.2705@gmail.com>
2017-05-26 14:21:08 +0300

    Added Vehicles, updated Ex03.xml

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Vehicle
+    {
+        private string m_LicensePlate;
+        private string m_ModelName;
+        private float m_EnergyRemaining;
+        private float m_MaxEnery;
+        private Dictionary<string, Wheel> m_Wheels;
+    }
+}
